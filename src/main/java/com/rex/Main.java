2a27a4d27d603715bacdeabb4a7e0ed747e108e8
Liu <5467193+zjuliuj05@users.noreply.github.com>
2019-09-30 23:02:34 +0800

    Convert to gradle project

--- src/main/java/com/rex/Main.java
+++ src/main/java/com/rex/Main.java
@@ -2,7 +2,11 @@ package com.rex;
 
 public class Main {
 
+    private static final Logger sLogger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
 	// write your code here
+
+        sLogger.
     }
 }
