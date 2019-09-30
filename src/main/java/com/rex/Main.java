3e0c5daf6ef86b6978f709a3b48930f6b3bff5c7
Liu <5467193+zjuliuj05@users.noreply.github.com>
2019-09-30 23:24:32 +0800

    Introduce slf4j

--- src/main/java/com/rex/Main.java
+++ src/main/java/com/rex/Main.java
@@ -1,12 +1,13 @@
 package com.rex;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
     private static final Logger sLogger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-	// write your code here
-
-        sLogger.
+        sLogger.info("");
     }
 }
