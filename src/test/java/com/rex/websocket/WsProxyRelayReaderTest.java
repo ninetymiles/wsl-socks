26ca6e73b43546bcc2702548a4422abf8a70ff5c
Liu <5467193+zjuliuj05@users.noreply.github.com>
2019-10-20 20:12:08 +0800

    Add large buffer test

--- src/test/java/com/rex/websocket/WsProxyRelayReaderTest.java
+++ src/test/java/com/rex/websocket/WsProxyRelayReaderTest.java
@@ -7,10 +7,10 @@ import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
-// TODO: Test byte buffer size > 65535, should auto split and join
 public class WsProxyRelayReaderTest {
 
     @Test
@@ -27,6 +27,39 @@ public class WsProxyRelayReaderTest {
                 .newDecoder()
                 .decode(data.nioBuffer())
                 .toString());
+
+        inbound.close();
+        outbound.close();
+    }
+
+    @Test
+    public void testLargeBinaryFrameToBuffer() throws Exception {
+        EmbeddedChannel inbound = new EmbeddedChannel();
+        EmbeddedChannel outbound = new EmbeddedChannel();
+        WsProxyRelayReader reader = new WsProxyRelayReader(outbound);
+
+        inbound.pipeline().addLast(reader);
+
+        StringBuffer sb = new StringBuffer();
+        int total = 65536 * 2;
+        for (int i = 0; i < total; i++) {
+            sb.append((char) ((i % 26) + 'A'));
+        }
+        inbound.writeInbound(new BinaryWebSocketFrame(Unpooled.wrappedBuffer(sb.toString().getBytes())));
+
+        ByteBuf data = outbound.readOutbound();
+
+        int idx = 0; // The first byte
+        assertEquals((idx % 26) + 'A', data.nioBuffer().get(idx));
+
+        idx = sb.length() - 1; // The last byte
+        assertEquals((idx % 26) + 'A', data.nioBuffer().get(idx));
+
+        Random rand = new Random();
+        for (int i = 0; i < 9; i++) {
+            idx = rand.nextInt(total);
+            assertEquals((idx % 26) + 'A', data.nioBuffer().get(idx));
+        }
 
         inbound.close();
         outbound.close();
--- src/test/java/com/rex/websocket/WsProxyRelayWriterTest.java
+++ src/test/java/com/rex/websocket/WsProxyRelayWriterTest.java
@@ -1,11 +1,14 @@
 package com.rex.websocket;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,5 +28,40 @@ public class WsProxyRelayWriterTest {
                 .newDecoder()
                 .decode(frame.content().nioBuffer())
                 .toString());
+    }
+
+
+    @Test
+    public void testLargeBufferToBinaryFrame() throws Exception {
+        EmbeddedChannel inbound = new EmbeddedChannel();
+        EmbeddedChannel outbound = new EmbeddedChannel();
+        WsProxyRelayWriter reader = new WsProxyRelayWriter(outbound);
+
+        inbound.pipeline().addLast(reader);
+
+        StringBuffer sb = new StringBuffer();
+        int total = 65536 * 2;
+        for (int i = 0; i < total; i++) {
+            sb.append((char) ((i % 26) + 'A'));
+        }
+        inbound.writeInbound(Unpooled.wrappedBuffer(sb.toString().getBytes()));
+
+        BinaryWebSocketFrame frame = outbound.readOutbound();
+        ByteBuffer data = frame.content().nioBuffer();
+
+        int idx = 0; // The first byte
+        assertEquals((idx % 26) + 'A', data.get(idx));
+
+        idx = sb.length() - 1; // The last byte
+        assertEquals((idx % 26) + 'A', data.get(idx));
+
+        Random rand = new Random();
+        for (int i = 0; i < 9; i++) {
+            idx = rand.nextInt(total);
+            assertEquals((idx % 26) + 'A', data.get(idx));
+        }
+
+        inbound.close();
+        outbound.close();
     }
 }
