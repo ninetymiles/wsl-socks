18345496ffe867e63d8abb9f78cecff24f0dd2c7
Liu <5467193+zjuliuj05@users.noreply.github.com>
2020-01-04 13:45:35 +0800

    Update the handshake protocol, reduce the trip around, avoid affect the connection performance too much

--- src/main/java/com/rex/wsproxy/websocket/control/ControlMessage.java
+++ src/main/java/com/rex/wsproxy/websocket/control/ControlMessage.java
@@ -4,39 +4,38 @@ package com.rex.wsproxy.websocket.control;
  * WebSocket control message
  *
  * 1. Handshake
- * If authentication enabled, server will send challenge message first when client connected.
- * client must use secret key (e.g. a UUID string) to generate hash token for ack immediately.
- * TOKEN = sha256(NONCE + SECRET)
- * e.g.
- * S -> C {'type':'challenge', 'action':'NONCE'}
- * C -> S {'type':'ack', 'action':'TOKEN'}
+ * Server will send hello message when client connected.
+ * S -> C {'type':'hello'}
  *
- * When auth success or do not require for auth, server will send ready message and start accepting proxy request
- * e.g.
- * S -> C {'type':'status', 'action':'ready'}
- *
- * If auth failed, server will send reject message, and force shutdown the socket after 3 seconds timeout
- * client should close socket immediately when receive the message to avoid server TIME-WAIT
- * e.g.
- * S -> C {'type':'status', 'action':'reject'}
+ * If authentication enabled, server will send with auth algorithm and nonce, default hs256 (HmacSHA256)
+ * S -> C {'type':'hello', 'action':'hs256', 'token':'NONCE'}
  *
  * 2. Proxy
- * When server ready, client can start request for proxy connection.
- * e.g.
+ * When handshake completed, client can start request for proxy connection.
  * C -> S {'type':'request', 'action':'connect', 'address':'www.google.com', 'port':'443'}
+ *
+ * If authentication enabled, client send request message should also include auth token
+ * C -> S {'type':'request', 'action':'connect', 'address':'www.google.com', 'port':'443', 'token':'TOKEN'}
+ *
+ * TOKEN = HMAC.init(SECRET).update(NONCE).update(address).update(port)
+ *
+ * If token valid and proxy connection success, server will send success response
  * S -> C {'type':'response', 'action':'success'}
  *
  * After success response, all the BinaryWebSocketFrame traffics will tunnel between client and proxy host
  *
- * If proxy connection failed, or server not ready, server will send failure response, and force shutdown the socket after 3 seconds timeout,
+ * If proxy connection failed, server will send failure response, and force shutdown the socket after 3 seconds timeout,
  * client should close the socket immediately when receive the message.
- * e.g.
  * S -> C {'type':'response', 'action':'failure'}
+ *
+ * If token auth failed, server will send reject response, and force shutdown the socket after 3 seconds
+ * S -> C {'type':'response', 'action':'reject'}
  */
 public class ControlMessage {
     public String type;
     public String action;
 
+    public String token;
     public String address;
     public int port;
 }
