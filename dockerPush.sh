2b3c64d88e14078e92369a5589d38e05a8795c25
Liu <5467193+ninetymiles@users.noreply.github.com>
2025-07-19 12:06:47 +0800

    Refine the docker script

---
+++ dockerPush.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Tag docker image wsl-socks:latest as version in labels
+
+VER=$(docker inspect  --format '{{.Config.Labels.version}}' wsl-socks)
+
+docker tag wsl-socks wsl-socks:${VER}
+docker push wsl-socks:${VER}
+docker push wsl-socks # latest
--- dockerRun.sh
+++ dockerRun.sh
@@ -4,3 +4,9 @@
 VER=$(docker inspect  --format '{{.Config.Labels.version}}' wsl-socks)
 
 docker tag wsl-socks wsl-socks:${VER}
+docker run -d \
+	-p 1080:1080 \
+	--restart always \
+	--name wsl-socks \
+	--env JAVA_OPTS="-Xms64m -Xmx1024m" \
+	wsl-socks
