9d26eed7a8a1c5ab2e1c249a7c5fcff92eeb8a6a
Liu <5467193+zjuliuj05@users.noreply.github.com>
2025-07-30 19:59:23 +0800

    Refine RelayHandler

--- src/main/java/com/rex/proxy/socks/RelayHandler.java
+++ src/main/java/com/rex/proxy/socks/RelayHandler.java
@@ -9,21 +9,27 @@ import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Bridge all data to target channel
+ * TODO: Rename as ChannelHandlerBridge
+ */
 public final class RelayHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger sLogger = LoggerFactory.getLogger(RelayHandler.class);
 
-    private final Channel mRelay;
+    private final Channel mOutput;
 
-    public RelayHandler(Channel channel) {
-        mRelay = channel;
+    public RelayHandler(Channel ch) {
+        sLogger.trace("<init> ch=<{}>", ch);
+        mOutput = ch;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        sLogger.trace("msg={}", msg);
         ReferenceCountUtil.retain(msg);
-        if (mRelay.isActive()) {
-            mRelay.writeAndFlush(msg);
+        if (mOutput.isActive()) {
+            mOutput.writeAndFlush(msg);
         }
     }
 
@@ -32,8 +38,8 @@ public final class RelayHandler extends ChannelInboundHandlerAdapter {
         // ctx: [id: 0x0182c0ea, L:/127.0.0.1:1080 - R:/127.0.0.1:54536]
         // cause: java.io.IOException: Connection reset by peer
         sLogger.warn("{} - {}", ctx.channel(), cause.getMessage());
-        if (mRelay.isActive()) {
-            mRelay.writeAndFlush(Unpooled.EMPTY_BUFFER)
+        if (mOutput.isActive()) {
+            mOutput.writeAndFlush(Unpooled.EMPTY_BUFFER)
                     .addListener(ChannelFutureListener.CLOSE);
         }
     }
