b0b1bc506fb1ca44a14a888e208354205d6d5d0e
Liu <5467193+zjuliuj05@users.noreply.github.com>
2025-08-07 17:05:55 +0800

    Update test case for http server path interceptor

--- src/test/java/com/rex/proxy/http/HttpServerPathInterceptorTest.java
+++ src/test/java/com/rex/proxy/http/HttpServerPathInterceptorTest.java
@@ -1,17 +1,22 @@
 package com.rex.proxy.http;
 
+import com.google.gson.Gson;
 import com.rex.proxy.WslLocal;
 import com.rex.proxy.utils.EchoServer;
+import com.rex.proxy.websocket.control.ControlMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.handler.codec.http.*;
+import okhttp3.Response;
+import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -103,6 +108,7 @@ public class HttpServerPathInterceptorTest {
 
     @Test
     public void testBridgeWebsocket() throws Exception {
+        Gson gson = new Gson();
         WebSocketListener listener = mock(WebSocketListener.class);
         MockWebServer server = new MockWebServer();
         server.enqueue(new MockResponse()
@@ -117,14 +123,38 @@ public class HttpServerPathInterceptorTest {
         channel.pipeline()
                 .addLast(new HttpServerPathInterceptor(config).group(group));
 
-        String url = config.proxyUri.getHost() + ":" + config.proxyUri.getPort();
-        FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.CONNECT, url);
-        request.headers().set("HOST", config.proxyUri.getHost());
+        String targetHost = "some.host";
+        int targetPort = 12345;
+        FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.CONNECT, targetHost + ":" + targetPort);
+        request.headers().set("HOST", targetHost);
         sLogger.trace("Send request to proxy server");
         channel.writeInbound(request);
-        verify(listener, timeout(3000)).onOpen(any(), any());
-        Thread.sleep(1000); // Let server can handle the RemoteReady event and send response
 
+
+        ArgumentCaptor<WebSocket> ws = ArgumentCaptor.forClass(WebSocket.class);
+        verify(listener, timeout(3000)).onOpen(ws.capture(), any());
+
+        // WslServer handshake without auth
+        ControlMessage msg = new ControlMessage();
+        msg.type = "hello";
+        ws.getValue().send(gson.toJson(msg));
+
+        // WslLocal request tunnel to target address
+        ArgumentCaptor<String> ack = ArgumentCaptor.forClass(String.class);
+        verify(listener, timeout(3000)).onMessage(any(), ack.capture());
+        msg = gson.fromJson(ack.getAllValues().get(0), ControlMessage.class);
+        assertEquals("request", msg.type);
+        assertEquals("connect", msg.action);
+        assertEquals(targetHost, msg.address);
+        assertEquals(Integer.valueOf(targetPort), msg.port);
+
+        // WslServer accept the request to allow WslLocal set up the bridge
+        msg = new ControlMessage();
+        msg.type = "response";
+        msg.action = "success";
+        ws.getValue().send(gson.toJson(msg));
+
+        Thread.sleep(1000); // Let server can handle the RemoteReady event and send response
         sLogger.trace("Read response from proxy server");
         FullHttpResponse response = channel.readOutbound();
         assertEquals(200, response.status().code());
